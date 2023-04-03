8ca8d1e9bde22fe61dd80bd93fc725ba3b542c04
Federico Macías <fma.abogado@gmail.com>
2023-04-03 08:28:09 -0600

    Finished the third row and working on the predictions page using regression

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
 import Dashboard from "@/scenes/dashboard";
-
+import Predictions from "./scenes/predictions";
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
@@ -17,10 +17,7 @@ function App() {
             <Navbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route
-                path="/predictions"
-                element={<div>predictions page</div>}
-              />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>
